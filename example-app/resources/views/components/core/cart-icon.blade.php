3b04c0692883783eb9190dd90d1d8701c0663d7d
sylvsetergodsp <sylvestergodspower62@gmail.com>
2024-07-21 17:00:52 +0100

    cart icon

--- example-app/resources/views/components/core/cart-icon.blade.php
+++ example-app/resources/views/components/core/cart-icon.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+<?php
+
+namespace App\View\Components\core;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class CartIcon extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.core.cart-icon');
+    }
+}
